b3d77201d91d69a6fc9c69fa0b257cc39d9cda98
bytemehardest <neeleshroy2024@outlook.com>
2024-05-25 14:42:20 +0530

    Sum

--- challenges/src/main/java/org/example/Basic.java
+++ challenges/src/main/java/org/example/Basic.java
@@ -6,7 +6,10 @@ public class Basic {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
 
+        System.out.format("Enter a number: ");
         int a = in.nextInt();
+
+        System.out.format("Enter another number: ");
         int b = in.nextInt();
 
         int sum = a + b;
