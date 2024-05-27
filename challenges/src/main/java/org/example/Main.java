8a57c52a06cbb1eef53b5310c93b11243b9ed823
bytemehardest <neeleshroy2024@outlook.com>
2024-05-27 18:50:16 +0530

    Resetting basics

--- challenges/src/main/java/org/example/Main.java
+++ challenges/src/main/java/org/example/Main.java
@@ -1,18 +1,7 @@
 package org.example;
 
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
-        Rectangle r = new Rectangle();
-        Scanner in = new Scanner(System.in);
-
-        System.out.println("Enter Length: ");
-        r.setLength(in.nextDouble());
-
-        System.out.println("Enter Breadth: ");
-        r.setBreadth(in.nextDouble());
-
-        System.out.println("Area of the rectangle: " + r.calculateArea());
+        System.out.println("Welcome");
     }
 }
